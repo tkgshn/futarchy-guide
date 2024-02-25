453a514a402de542e2bb79668327f79fdd5f47eb
agrippa kellum <ask252@cornell.edu>
2024-02-25 17:45:38 -0500

    fucking around

--- src/app/coinsplit/coinsplit2/page.tsx
+++ src/app/coinsplit/coinsplit2/page.tsx
@@ -48,6 +48,7 @@ export function Coin({
           M
         </text>
       </mask>
+
       <circle
         className={"fill-black"}
         cx="105"
@@ -56,7 +57,14 @@ export function Coin({
         //style={{ mixBlendMode: "multiply", mask: "url(#mask)" }}
       />
       <circle
-        className={condition === "pass" ? "fill-lime-500" : "fill-red-500"}
+        cx="105"
+        cy="105"
+        r="104"
+        fill={condition === "pass" ? "#0ff" : "#f0f"}
+        //style={{ mixBlendMode: "multiply", mask: "url(#mask)" }}
+      />
+      <circle
+        className={"fill-blue-600 mix-blend-multiply"}
         cx="105"
         cy="105"
         r="100"
@@ -112,111 +120,10 @@ export function Coinsplit({ active }: { active?: boolean }) {
   )
 }
 
-export function USDCSplit({ active }: { active?: boolean }) {
-  const [open, toggle] = useState(false)
-
-  const peekRef = useSpringRef()
-  const peek = useSpring({
-    ref: peekRef,
-    width: open ? 100 : 0,
-    config: { friction: open ? 10 : undefined },
-  })
-
-  const pookRef = useSpringRef()
-  const pook = useSpring({ ref: pookRef, width: open ? 500 : 200 })
-
-  useChain(open ? [peekRef, pookRef] : [pookRef, peekRef], [0, open ? 0.6 : 0])
-
-  const radius = 90
-  const circumference = 2 * Math.PI * radius
-  const dashCount = 80 // Choose the number of dashes you want
-  const dasharray = circumference / dashCount
-
-  const MASK_ID = "usdcMask"
-
-  return (
-    <div
-      className="flex flex-row items-center"
-      onClick={active ? () => toggle(!open) : undefined}
-    >
-      <animated.div style={peek}>
-        <svg viewBox="0 0 200 240" style={{ width: "200px", height: "240px" }}>
-          <mask id={MASK_ID}>
-            <rect width="200" height="240" fill="white" />
-            <circle
-              cx="100"
-              cy="100"
-              r={radius}
-              stroke="black"
-              fill="none"
-              strokeWidth="5"
-              strokeDasharray={`${dasharray}`}
-            />
-            <text
-              x="50%"
-              y="45%"
-              textAnchor="middle"
-              dominantBaseline="middle"
-              fontSize="120"
-              fontWeight="bold"
-              fontFamily="monospace"
-              fill="black"
-            >
-              $
-            </text>
-          </mask>
-          <circle
-            className="fill-cyan-500"
-            cx="100"
-            cy="100"
-            r="100"
-            style={{ mixBlendMode: "multiply", mask: `url(#${MASK_ID})` }}
-          />
-          <text
-            x="50%"
-            y="220"
-            textAnchor="middle"
-            className="fill-cyan-500 font-mono"
-            style={{ mixBlendMode: "multiply", fontSize: "20px" }}
-          >
-            1 {open ? "p" : ""}USDC
-          </text>
-        </svg>
-      </animated.div>
-      <animated.div style={pook} className="flex flex-row justify-end">
-        <div>
-          <svg
-            viewBox="0 0 200 240"
-            style={{ width: "200px", height: "240px" }}
-          >
-            <circle
-              className="fill-purple-500"
-              cx="100"
-              cy="100"
-              r="100"
-              style={{ mixBlendMode: "multiply", mask: `url(#${MASK_ID})` }}
-            />
-            <text
-              x="50%"
-              y="220"
-              textAnchor="middle"
-              className="fill-purple-500 font-mono"
-              style={{ mixBlendMode: "multiply", fontSize: "20px" }}
-            >
-              1 {open ? "f" : ""}USDC
-            </text>
-          </svg>
-        </div>
-      </animated.div>
-    </div>
-  )
-}
-
 export default function Fart() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
       <Coinsplit active />
-      <USDCSplit active />
     </main>
   )
 }
