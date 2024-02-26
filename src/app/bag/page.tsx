c3db64fbe60e73bca596713d9d1648e15a87affc
agrippa kellum <ask252@cornell.edu>
2024-02-26 12:47:20 -0500

    bag emitter

--- src/app/bag/page.tsx
+++ src/app/bag/page.tsx
@@ -1,63 +1,6 @@
 "use client"
-import { MutableRefObject, ReactNode, useEffect, useRef, useState } from "react"
-import { useSpring, animated } from "@react-spring/web"
-import React from "react"
-
-const Particle = ({
-  to: [toX, toY],
-  from: [fromX, fromY],
-  children,
-  onResolve,
-  id,
-}: {
-  to: [x: string, y: string]
-  from: [x: string, y: string]
-  children: ReactNode
-  onResolve?: () => void
-  id: string
-}) => {
-  console.log(" i rerendered!")
-  const spring = useSpring({
-    from: { opacity: 0, left: fromX, top: fromY },
-    to: async (next, cancel) => {
-      console.log("bagool", id)
-      await next({ opacity: 1, left: toX, top: toY })
-      console.log("beep", id)
-      onResolve?.()
-    },
-    //config: { tension: 200, friction: 20 },
-  }) /* 
-  useEffect(() => {
-    const f = async () => {
-      const a = api.start(() => ({ left: toX, top: toY }))
-      console.log("a", a)
-      await a[0]
-      console.log(id, "fulffillment!", new Date().toISOString())
-      onResolve?.()
-    }
-    f()
-  }, [api, id, onResolve, toX, toY]) */
-
-  useEffect(() => {
-    console.log("i rerendered cause of children")
-  }, [children])
-
-  return (
-    <>
-      <animated.div className={"absolute"} style={spring}>
-        {children}
-      </animated.div>
-    </>
-  )
-}
-
-const MemoParticle = React.memo(Particle)
-
-type EmitterArgs = {
-  source: [x: string, y: string]
-  target: [x: string, y: string]
-  particle: ReactNode
-}
+import { MutableRefObject, ReactNode, useRef, useState } from "react"
+import { animated, useTransition } from "@react-spring/web"
 
 const Emitter = ({
   emitRef,
@@ -72,6 +15,17 @@ const Emitter = ({
 }) => {
   const [inFlight, setInFlight] = useState<ThingyInFlight[]>([])
 
+  const transitions = useTransition(inFlight, {
+    from: { opacity: 0, left: source[0], top: source[1] },
+    to: { opacity: 1, left: target[0], top: target[1] },
+    enter: (id) => async (next, cancel) => {
+      console.log("entering", id)
+      await next({ opacity: 1, left: target[0], top: target[1] })
+      setInFlight((prev) => prev.filter((x) => x.key !== id.key))
+    },
+    //leave: { opacity: 1 },
+  })
+
   const emit = () => {
     const id = Math.random().toString(36).substring(2, 8)
     setInFlight((prev) => [...prev, { key: id }])
@@ -82,23 +36,10 @@ const Emitter = ({
 
   return (
     <>
-      {inFlight.map((thingy) => (
-        <MemoParticle
-          key={thingy.key}
-          id={thingy.key.toString()}
-          to={target}
-          from={source}
-          onResolve={() =>
-            setInFlight((prev) => {
-              console.log("purging", thingy.key, prev)
-              const next = prev.filter((x) => x.key !== thingy.key)
-              console.log(next)
-              return next
-            })
-          }
-        >
+      {transitions((style) => (
+        <animated.div className={"absolute"} style={style}>
           {particle}
-        </MemoParticle>
+        </animated.div>
       ))}
     </>
   )
@@ -121,7 +62,7 @@ const FillableBag = ({
 }) => {
   const emitRef = useRef<null | (() => void)>(null)
   const [nummies, setNummies] = useState(0)
-
+  /* 
   const [spring, api] = useSpring(
     () => ({
       top: bagY,
@@ -130,7 +71,7 @@ const FillableBag = ({
       config: { tension: 200, friction: 20 },
     }),
     [nummies]
-  )
+  ) */
 
   return (
     <>
@@ -142,7 +83,7 @@ const FillableBag = ({
       />
       <animated.div
         className={"absolute"}
-        style={spring}
+        //style={spring}
         onClick={() => emitRef.current?.()}
       >
         {bag}
@@ -158,7 +99,7 @@ export default function Fart() {
         <FillableBag
           bagPosition={["50%", "50%"]}
           targetPosition={["30%", "25%"]}
-          bag={<div className="scale-[10] opacity-10">🛍️</div>}
+          bag={<div className="opacity-10 scale-[10]">🛍️</div>}
           thingy={<div className="">🍬</div>}
           thingies={5}
         />
