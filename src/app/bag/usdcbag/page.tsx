ff796095a22410879fd9f0558e2c16f929aace8e
agrippa kellum <ask252@cornell.edu>
2024-02-27 15:28:08 -0500

    usdc wallet spring

--- src/app/bag/usdcbag/page.tsx
+++ src/app/bag/usdcbag/page.tsx
@@ -1,8 +1,8 @@
-import { Market } from "@/app/market/page"
+"use client"
 import { DemoZone } from "@/app/track/intro/page"
-import { COIN_COLOR } from "@/constants"
+import { USDC_COLOR } from "@/constants"
 import { animated, useSpring } from "@react-spring/web"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 function useSpringEnter() {
   const [spring, api] = useSpring(() => ({ from: { opacity: 0, y: -500 } }), [])
@@ -12,14 +12,21 @@ function useSpringEnter() {
   return spring
 }
 
-export function MisterMarket() {
+export function USDCBag({ amount }: { amount: number }) {
   const spring = useSpringEnter()
+
+  const { amountSpring, x } = useSpring({
+    amountSpring: amount,
+    x: amount / 10000,
+  })
+
   return (
     <animated.div style={spring}>
       <div
-        className="bg-zinc-600 rounded-xl border-zinc-300 flex flex-col justify-center items-center font-mono text-center select-none"
+        className="rounded-full border-zinc-300 flex flex-col justify-center items-center font-mono text-center select-none"
         style={{
           position: "absolute",
+          background: USDC_COLOR,
           top: "0",
           left: "50%",
           transform: "translate(-50%, -50%)",
@@ -28,9 +35,11 @@ export function MisterMarket() {
           height: "250px",
         }}
       >
-        <div className="text-5xl mb-3">the market</div>
-        <div>
-          1 <span className={`text-[${COIN_COLOR}]`}>META</span> :: 49,000 USDC
+        <div className="text-5xl mb-3">
+          $
+          <animated.span style={{ x }}>
+            {amountSpring.to((x) => x.toFixed(0))}
+          </animated.span>
         </div>
       </div>
     </animated.div>
@@ -38,10 +47,15 @@ export function MisterMarket() {
 }
 
 export default function Home() {
+  const [toggle, setToggle] = useState(false)
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+    <main
+      className="flex min-h-screen flex-col items-center justify-center p-24"
+      onClick={() => setToggle(!toggle)}
+    >
       <DemoZone>
-        <Market step={4} />
+        <USDCBag amount={49000 * (toggle ? 3 : 2)} />
       </DemoZone>
     </main>
   )
