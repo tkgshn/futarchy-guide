cbe618863364154e3c70a76e1bf4b561311b266d
agrippa kellum <ask252@cornell.edu>
2024-02-28 22:19:11 -0500

    fix opengraph hopefully

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ const font = Courier_Prime({ subsets: ["latin"], weight: "400" })
 export const metadata: Metadata = {
   title: "futarchy.guide",
   description: "You have a lot to learn, cyberanon.",
+  metadataBase: new URL("https://futarchy.guide"), // why would this be needed?? fuck you netlify
 }
 
 export default function RootLayout({
