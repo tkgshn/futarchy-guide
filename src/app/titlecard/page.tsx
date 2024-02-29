684ce99120a0f7b3e22f516aa13cea246bec03d8
agrippa kellum <ask252@cornell.edu>
2024-02-28 19:14:08 -0500

    attempt og image

---
+++ src/app/titlecard/page.tsx
@@ -0,0 +1,9 @@
+export default function Card() {
+  return (
+    <main className="flex min-h-screen items-center justify-center text-[90px] p-24">
+      futarchy
+      <div className="text-lime-500 mix-blend-lighten w-2">.guide</div>
+      <div className="text-red-500 mix-blend-lighten w-0">.guide</div>
+    </main>
+  )
+}
